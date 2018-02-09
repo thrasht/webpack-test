1deef2136619c5cc9a078b554b3c0e8140dcb341
thrasht <eduardo.mhdz@gmail.com>
2018-02-08 19:05:25 -0600

    Loaders

---
+++ css-style-loader/index.js
@@ -0,0 +1,5 @@
+
+import './my-style.css'
+
+document.write('Hola mundo');
+console.log('Hola mundo!, desde webpack en un webpack.config external');
---
+++ css-style-loader/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			//Loaders
+			{
+				test: /\.css$/,
+				use: ['style-loader','css-loader'],//loader para el archivo
+			}
+		]
+	}
+}
